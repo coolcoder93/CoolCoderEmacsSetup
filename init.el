15ada85f91bccbea102be47389050801f7d3e911
CoolCoder93 <96700307+coolcoder93@users.noreply.github.com>
2022-07-17 19:41:30 +0200

    Update init.el

--- init.el
+++ init.el
@@ -182,5 +182,10 @@
 (use-package treemacs-icons-dired
   :hook (dired-mode . treemacs-icons-dired-enable-once)
   :ensure t)
+;; Ruby mode
+(add-to-list 'auto-mode-alist
+             '("\\.\\(?:cap\\|gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist
+             '("\\(?:Brewfile\\|Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
 
 ;;; init.el ends here
